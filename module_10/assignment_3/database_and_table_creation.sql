c1db61985cfc9b4dc3c838c60f11d044fd8150e7
Amro <amro.s.taha@gmail.com>
2023-07-23 02:50:57 -0500

    7 7

--- module_10/assignment_3/database_and_table_creation.sql
+++ module_10/assignment_3/database_and_table_creation.sql
@@ -1,61 +1,122 @@
-""" 
-    Title: pysports_join_queries.py
-    Author: Amro Taha
-    Date: 07 July 2023
-    Description: Database and Table Creation
-"""
 
-mysql> CREATE TABLE user (
-    user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(75) NOT NULL,
-    last_name VARCHAR(75) NOT NULL
+-- create whatabook_user and grant them all privileges to the whatabook database 
+CREATE USER root@'localhost' IDENTIFIED WITH mysql_native_password BY 'snow';
+
+-- grant all privileges to the whatabook database to user whatabook_user on localhost 
+GRANT ALL PRIVILEGES ON whatabook.* TO'root'@'localhost';
+
+-- drop contstraints if they exist
+ALTER TABLE wishlist DROP FOREIGN KEY fk_book;
+ALTER TABLE wishlist DROP FOREIGN KEY fk_user;
+
+-- drop tables if they exist
+DROP TABLE IF EXISTS store;
+DROP TABLE IF EXISTS book;
+DROP TABLE IF EXISTS wishlist;
+DROP TABLE IF EXISTS user;
+
+/*
+    Create table(s)
+*/
+CREATE TABLE store (
+    store_id    INT             NOT NULL    AUTO_INCREMENT,
+    locale      VARCHAR(500)    NOT NULL,
+    PRIMARY KEY(store_id)
 );
 
-
-mysql> CREATE TABLE wishlist (
-    wishlist_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    book_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user (user_id),
-    FOREIGN KEY (book_id) REFERENCES book (book_id)
+CREATE TABLE book (
+    book_id     INT             NOT NULL    AUTO_INCREMENT,
+    book_name   VARCHAR(200)    NOT NULL,
+    author      VARCHAR(200)    NOT NULL,
+    details     VARCHAR(500),
+    PRIMARY KEY(book_id)
 );
 
-
-mysql> CREATE TABLE book (
-    book_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    book_name VARCHAR(200) NOT NULL,
-    details VARCHAR(500),
-    author VARCHAR(200) NOT NULL
+CREATE TABLE user (
+    user_id         INT         NOT NULL    AUTO_INCREMENT,
+    first_name      VARCHAR(75) NOT NULL,
+    last_name       VARCHAR(75) NOT NULL,
+    PRIMARY KEY(user_id) 
 );
 
-
-mysql> CREATE TABLE store (
-    store_id INT NOT NULL PRIMARY KEY,
-    locale VARCHAR(500) NOT NULL
+CREATE TABLE wishlist (
+    wishlist_id     INT         NOT NULL    AUTO_INCREMENT,
+    user_id         INT         NOT NULL,
+    book_id         INT         NOT NULL,
+    PRIMARY KEY (wishlist_id),
+    CONSTRAINT fk_book
+    FOREIGN KEY (book_id)
+        REFERENCES book(book_id),
+    CONSTRAINT fk_user
+    FOREIGN KEY (user_id)
+        REFERENCES user(user_Id)
 );
 
+/*
+    insert store record 
+*/
+INSERT INTO store(locale)
+    VALUES('1000 Galvin Rd S, Bellevue, NE 68005');
 
-mysql> INSERT INTO store (locale) VALUES ('123 Main Street, Anytown, CA 91234');
-Query OK, 1 row affected (0.00 sec)
-INSERT INTO store (locale) VALUES ('123 Main Street, Anytown, CA 91234');
+/*
+    insert book records 
+*/
+INSERT INTO book(book_name, author, details)
+    VALUES('The Return of the King', 'J.R.Tolkien', 'The third part of The Lord of the Rings');
 
-INSERT INTO book (book_name, details, author) VALUES
-('The Hitchhiker''s Guide to the Galaxy', 'A comedy science fiction series created by Douglas Adams.', 'Douglas Adams'),
-('Pride and Prejudice', 'A romantic novel of manners written by Jane Austen.', 'Jane Austen'),
-('The Lord of the Rings', 'An epic high fantasy trilogy written by J. R. R. Tolkien.', 'J. R. R. Tolkien'),
-('Harry Potter and the Sorcerer''s Stone', 'A fantasy novel by J. K. Rowling.', 'J. K. Rowling'),
-('The Hunger Games', 'A science fiction novel by Suzanne Collins.', 'Suzanne Collins'),
-('The Book Thief', 'A historical novel by Markus Zusak.', 'Markus Zusak'),
-('The Fault in Our Stars', 'A young adult novel by John Green.', 'John Green'),
-('To Kill a Mockingbird', 'A novel by Harper Lee.', 'Harper Lee');
+INSERT INTO book(book_name, author, details)
+    VALUES('The Fellowship of the Ring', 'J.R.Tolkien', 'The first part of The Lord of the Rings');
 
-INSERT INTO user (first_name, last_name) VALUES
-('John', 'Doe'),
-('Jane', 'Doe'),
-('Mary', 'Doe');
+INSERT INTO book(book_name, author, details)
+    VALUES('The Two Towers', 'J.R.Tolkien', "The second part of The Lord of The Rings");
 
-INSERT INTO wishlist (user_id, book_id) VALUES
-(1, 1),
-(1, 2),
-(2, 3),
-(3, 4);
+INSERT INTO book(book_name, author)
+    VALUES('The Hobbit or There and Back Again', 'J.R.Tolkien');
+
+INSERT INTO book(book_name, author)
+    VALUES('Dune: Deluxe Edition', 'Frank Herbert');
+
+INSERT INTO book(book_name, author)
+    VALUES("Charlotee's Web", 'E.B. White');
+
+INSERT INTO book(book_name, author)
+    VALUES('The Great Gatsby', 'F. Scott Fitzgerald');
+
+INSERT INTO book(book_name, author)
+    VALUES('The Lion, the Witch, and the Wardrobe', 'C.S. Lewis');
+
+INSERT INTO book(book_name, author)
+    VALUES('The Catcher and the Rye', 'J.D. Salinger');
+
+/*
+    insert user
+*/ 
+INSERT INTO user(first_name, last_name) 
+    VALUES('Thorin', 'Oakenshield');
+
+INSERT INTO user(first_name, last_name)
+    VALUES('Bilbo', 'Baggins');
+
+INSERT INTO user(first_name, last_name)
+    VALUES('Frodo', 'Baggins');
+
+/*
+    insert wishlist records 
+*/
+INSERT INTO wishlist(user_id, book_id) 
+    VALUES (
+        (SELECT user_id FROM user WHERE first_name = 'Thorin'), 
+        (SELECT book_id FROM book WHERE book_name = 'The Hobbit or There and Back Again')
+    );
+
+INSERT INTO wishlist(user_id, book_id)
+    VALUES (
+        (SELECT user_id FROM user WHERE first_name = 'Bilbo'),
+        (SELECT book_id FROM book WHERE book_name = 'The Fellowship of the Ring')
+    );
+
+INSERT INTO wishlist(user_id, book_id)
+    VALUES (
+        (SELECT user_id FROM user WHERE first_name = 'Frodo'),
+        (SELECT book_id FROM book WHERE book_name = 'The Return of the King')
+    );
---
+++ module_10/assignment_3/mysqltest.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+def test_connection():
+    try:
+        config = {
+            "user": "root",
+            "password": "snow",
+            "host": "127.0.0.1",
+            "database": "pysports",
+            "port": "3306",
+            "raise_on_warnings": True
+        }
+        connection = mysql.connector.connect(**config)
+        print("Connection successful")
+    except mysql.connector.Error as e:
+        print(e)
+
+if __name__ == "__main__":
+    test_connection()
